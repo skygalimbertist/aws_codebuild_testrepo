86742c238319e093fedbc1de85704813f8f17dc0
Galimberti Stefano <stefano.galimberti.nttdata@skytv.it>
2020-12-23 10:08:08 +0100

    Add sources

---
+++ src/test/java/TestMessageUtil.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMessageUtil {
+
+  String message = "Robert";    
+  MessageUtil messageUtil = new MessageUtil(message);
+   
+  @Test
+  public void testPrintMessage() {      
+    System.out.println("Inside testPrintMessage()");     
+    assertEquals(message,messageUtil.printMessage());
+  }
+
+  @Test
+  public void testSalutationMessage() {
+    System.out.println("Inside testSalutationMessage()");
+    message = "Hi!" + "Robert";
+    assertEquals(message,messageUtil.salutationMessage());
+  }
+}
